68097ccb8e6c13dc1ff0a534a33e4f3551c0f870
Lynesth <lyn@nautile.nc>
2014-07-18 12:03:46 +1100

    Now using rem so it works no matter what

--- jquery.f5lcb.js
+++ jquery.f5lcb.js
@@ -20,26 +20,12 @@
 
 	F5LCB = clazz(Object, {
 
-		paddings: {
-			top: {
-				tiny: { top: "0.9rem", bottom: "0.4125rem" },
-				small: { top: "1.15rem", bottom: "0.6625rem" },
-				default: { top: "1.25rem", bottom: "0.8125rem" },
-				large: { top: "1.4rem", bottom: "0.9125rem" }
-			},
-			bottom: {
-				tiny: { bottom: "0.9rem", top: "0.4125rem" },
-				small: { bottom: "1.15rem", top: "0.6625rem" },
-				default: { bottom: "1.25rem", top: "0.8125rem" },
-				large: { bottom: "1.4rem", top: "0.9125rem" }
-			}
-		},
 
-
+		// Will think of a better way later ^^
 		progBarStyle: {
-			top: "position: absolute; height: 10px; top: 0; left: 0; width: 100%",
-			bottom: "position: absolute; height: 10px; bottom: 0; left: 0; width: 100%; margin: 0",
-			inner: "position: absolute; height: 10px; top: 0; left: 0; height: 100%; width: 100%; opacity: 0.3"
+			top: "position: absolute; height: 0.625rem; top: 0; left: 0; width: 100%",
+			bottom: "position: absolute; height: 0.625rem; bottom: 0; left: 0; width: 100%; margin: 0",
+			inner: "position: absolute; height: 0.625rem; top: 0; left: 0; height: 100%; width: 100%; opacity: 0.3"
 		},
 
 
@@ -61,15 +47,20 @@
 			}
 
 
-			// allowed styles : top, bottom, inner
+			// allowed styles : "top", "bottom", "inner"
 			self.style = options.style || "top";
 			self.timer = options.timer || 1000;
 			self.callback = options.callback || null;
 			self.progBarClass = options.progBarClass || "prog-bar";
 
+
 			if (self.style != "inner") {
-				self.element.css('padding-top', self.paddings[self.style][self.bClass].top)
-							.css('padding-bottom', self.paddings[self.style][self.bClass].bottom)
+				var rem = parseInt($('html').css('font-size'));
+				var diff = self.style == "top" ? 0.25 : -0.25;
+				var padTop = parseInt(self.element.css('padding-top')) / rem + diff;
+				var padBottom = parseInt(self.element.css('padding-bottom')) / rem - diff;
+				self.element.css('padding-top', padTop.toString()+"rem")
+							.css('padding-bottom', padBottom.toString()+"rem")
 							.css('vertical-align', 'bottom');
 			}
 
